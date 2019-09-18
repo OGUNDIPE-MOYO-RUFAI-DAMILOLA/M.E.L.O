e8e8a20e5c6e3b3c672153827ab2115fe21c5388
blak-beryl <54101436+blak-beryl@users.noreply.github.com>
2019-09-18 15:39:38 +0100

    second commit

---
+++ M.E.L.O/dashboard.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="image2.png" type="image/x-icon">
+  <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="index.css">
+  <title>MELO</title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg sticky-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="logo.png" width="50" height="50" class="d-inline-block align-top" alt="">
+        <span class="logo">M.E.L.O</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active mr-5">
+            <a class="nav-link text-white" href="#">Home</a>
+          </li>
+          <li class="nav-item mr-5">
+            <a class="nav-link text-white" href="#">MY MUSIC</a>
+          </li>
+          <li class="nav-item mr-5">
+            <a class="nav-link text-white" href="#">CONTACT US</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="burna.jpg" class="card-img-top fr" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="davido.jpg" class="card-img-top fr" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top fr" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top fr" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top fr" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <div class="row">
+    <div class="card card1" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card2" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+    <div class="card card3" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+     
+    </div>
+    <div class="card card4" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+    </div>
+    <div class="card card5" style="width: 16rem;">
+      <img src="image3.png" class="card-img-top" alt="...">
+   </div>
+  
+  </div>
+
+  <?php require 'footer2.php'; ?>
+
+  <script src="bootstrap-4.3.1-dist/js/jquery-3.4.1.min.js"></script>
+  <script src="bootstrap-4.3.1-dist/js/popper.min.js"></script>
+  <script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/js/mdb.min.js"></script>
+
+
+
+</body>
+
+</html>
